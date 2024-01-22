59136434d2961b161e900f7ca9833d20f72bd4c8
hisa11 <132685722+hisa11@users.noreply.github.com>
2024-01-22 15:48:53 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,55 @@
 #include "mbed.h"
+BufferedSerial pc(USBTX, USBRX, 250000); // パソコンとのシリアル通信
+CANMessage msg;
+CAN can1(PA_11, PA_12, (int)1e6);
+int16_t pwm0[4] = {0, 0, 0, 0}; // モタドラ1
+int sokudo = 1000;
 
-BufferedSerial pc(USBTX, USBRX, 115200); // パソコンとのシリアル通信
-CAN can(PA_11, PA_12, (int)1e6);
-uint8_t DATA[8] = {};
-int main(){
-    while(1){
-        int16_t output = 9000;
-        DATA[0] = output >> 1;
-        DATA[1] = output;
-        CANMessage msg0(0x200, DATA, 8);
-        can.write(msg0);
-
-        for(auto e: DATA) {
-            printf("%2x ", e);
+int main()
+{
+    while (1)
+    {
+        if (pc.readable())
+        {
+            char buf;
+            pc.read(&buf, sizeof(buf));
+            if (buf == 'w')
+            {
+                pwm0[0] = sokudo; // 右前
+                pwm0[1] = sokudo; // 右後ろ
+                pwm0[2] = sokudo; // 左前
+                pwm0[3] = sokudo; // 左後ろ
+            }
+            else if (buf == 's')
+            {
+                pwm0[0] = -sokudo;
+                pwm0[1] = -sokudo;
+                pwm0[2] = -sokudo;
+                pwm0[3] = -sokudo;
+            }
+            else if (buf == 'd')
+            {
+                pwm0[0] = sokudo;
+                pwm0[1] = -sokudo;
+                pwm0[2] = -sokudo;
+                pwm0[3] = sokudo;
+            }
+            else if (buf == 'a')
+            {
+                pwm0[0] = -sokudo;
+                pwm0[1] = sokudo;
+                pwm0[2] = sokudo;
+                pwm0[3] = -sokudo;
+            }
+            else if (buf == 'z')
+            {
+                pwm0[0] = 0;
+                pwm0[1] = 0;
+                pwm0[2] = 0;
+                pwm0[3] = 0;
+            }
         }
-        printf("\n");
+        CANMessage msg0(2, (const uint8_t *)pwm0, 8);
+        can1.write(msg0);
     }
 }
